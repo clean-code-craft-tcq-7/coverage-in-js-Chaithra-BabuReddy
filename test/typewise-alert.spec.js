6d0acdad16d6cd097f5f98fb63127ae2e0a881a1
Chaithra Babu Reddy <BabuReddy.Chaithra@in.bosch.com>
2023-02-19 21:46:50 +0530

    tests added

--- test/typewise-alert.spec.js
+++ test/typewise-alert.spec.js
@@ -34,13 +34,13 @@ describe('test to check HI_ACTIVE_COOLING', () => {
   it('test to check alert via controller', () => {
     const alertType = 'TO_CONTROLLER';
     const coolingType = 'HI_ACTIVE_COOLING';
-    let res = myLib.checkAndAlert(alertType, coolingType, -1);
+    let res = myLib.checkAndAlert(alertType, coolingType, -10);
     expect(res).equals(`TOO_LOW-${alertMessage.viaController}`);
 
-    let res1 = myLib.checkAndAlert(alertType, coolingType, 37);
+    let res1 = myLib.checkAndAlert(alertType, coolingType, 100);
     expect(res1).equals(`TOO_HIGH-${alertMessage.viaController}`);
 
-    let res2 = myLib.checkAndAlert(alertType, coolingType, 10);
+    let res2 = myLib.checkAndAlert(alertType, coolingType, 40);
     expect(res).equals(`NORMAL-${alertMessage.viaController}`);
   });
 
@@ -49,13 +49,13 @@ describe('test to check MED_ACTIVE_COOLING', () => {
   it('test to check alert via controller', () => {
     const alertType = 'TO_CONTROLLER';
     const coolingType = 'MED_ACTIVE_COOLING';
-    let res = myLib.checkAndAlert(alertType, coolingType, -1);
+    let res = myLib.checkAndAlert(alertType, coolingType, -20);
     expect(res).equals(`TOO_LOW-${alertMessage.viaController}`);
 
-    let res1 = myLib.checkAndAlert(alertType, coolingType, 37);
+    let res1 = myLib.checkAndAlert(alertType, coolingType, 70);
     expect(res1).equals(`TOO_HIGH-${alertMessage.viaController}`);
 
-    let res2 = myLib.checkAndAlert(alertType, coolingType, 10);
+    let res2 = myLib.checkAndAlert(alertType, coolingType, 30);
     expect(res).equals(`NORMAL-${alertMessage.viaController}`);
   });
 
